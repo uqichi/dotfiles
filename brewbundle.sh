84e14f8776a76743c425d8b79db40f6f2ee4e546
田村 優吉 <a13533@pc-8905.cyberagent.co.jp>
2015-06-23 19:01:11 +0900

    Add brew file

---
+++ brewbundle.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew doctor
+brew doctor
+
+# brew install
+brew install git
+brew install tmux
+brew install tree
+brew install w3m
+brew install zsh
+brew install caskroom/cask/brew-cask
+
+# brew cask install
+brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+brew cask install google-chrome
+brew cask install alfred
+brew cask install iterm2
+brew cask install dropbox
+brew cask install virtualbox
+brew cask install flux
+brew cask install evernote
+brew cask install sequel-pro
+brew cask install clipmenu
+brew cask install appcleaner
+brew cask install kobito
+brew cask install mysqlworkbench
+brew cask install sublime-text
+brew cask install mou
+brew cask install quicklook-json
+
+# install manually from App Store below
+# - Magnet
