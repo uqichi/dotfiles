32d2ed423d5e3dbea818ba6210b36715ad5df855
田村 優吉 <a13533@pc-9963.cyberagent.co.jp>
2015-06-29 16:47:54 +0900

    Fix

--- init.zsh.sh
+++ init.zsh.sh
@@ -3,23 +3,19 @@
 # Prezto installation
 # https://github.com/sorin-ionescu/prezto
 
-# 1. Launch Zsh:
-zsh
-
-# 2. Clone the repository:
+# Clone the repository:
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-# 3. Create a new Zsh configuration by copying the Zsh configuration files provided:
+# Create a new Zsh configuration by copying the Zsh configuration files provided:
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-# 4. Set Zsh as your default shell:
+# Set Zsh as your default shell:
+zsh
 chsh -s /bin/zsh
 
-# 5. Open a new Zsh terminal window or tab.
-
-
-# add source my zshrc
+# Add source
 echo "source $HOME/dotfiles/.zshrc" >> $HOME/.zshrc
+source $HOME/.zshrc
