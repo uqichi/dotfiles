868774e66a4d24f58cc81e2b1f842fa25ba9d2db
田村 優吉 <a13533@pc-9963.cyberagent.co.jp>
2015-08-28 12:34:09 +0900

    Add path

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 ## ----------------------------------------------------------------------
 brew update && brew cleanup
 
-BREW_APPS="tig git-now tmux reattach-to-user-namespace tree wget watch coreutils w3m zsh caskroom/cask/brew-cask"
+BREW_APPS="tig git-now tmux reattach-to-user-namespace tree wget watch coreutils w3m zsh caskroom/cask/brew-cask pwgen cowsay figlet fortune"
 
 for app in $BREW_APPS
 do
