760fe369b79c278b93975edfa34daa8ffa11c58e
Tamura Yukichi <tamura_yukichi@cyberagent.co.jp>
2016-10-21 15:44:03 +0900

    [Change] cut into _lndots.sh

--- _setup.sh
+++ _setup.sh
@@ -4,17 +4,7 @@
 sh _brew.sh
 sh _brewcask.sh
 sh _lnvim.sh
-
-# symlink dotfiles
-SRC_DIR=$(cd $(dirname $0) && pwd)
-DOTFILES="gitconfig gitignore tmux.conf vimrc vrapperrc xvimrc zshrc.me"
-for file in $DOTFILES
-do
-  ln -s ${SRC_DIR}/${file} ~/.${file}
-done
-
-# symlink dash app library
-open /Applications/Dash.app && ln -sfn ${SRC_DIR}/dash/library.dash ~/Library/Application\ Support/Dash/library.dash
+sh _lndots.sh
 
 # copy tmuxinator dir
 cp -r ${SRC_DIR}/tmuxinator ~/.tmuxinator
